e0908028fb2ed40a6c68e3f5c485eeb62553d7ed
cabbachew <102335309+cabbachew@users.noreply.github.com>
2023-02-19 20:38:10 -0800

    throttle high number of concurrent requests using lodash debounce

--- app/javascript/controllers/form_controller.js
+++ app/javascript/controllers/form_controller.js
@@ -1,7 +1,12 @@
 import { Controller } from "@hotwired/stimulus"
+import debounce from "https://cdn.skypack.dev/lodash.debounce"
 
 export default class extends Controller {
   static get targets() { return ["submit"] }
+
+  initialize() {
+    this.submit = debounce(this.submit.bind(this), 200)
+  }
 
   connect() {
     this.submitTarget.hidden = true

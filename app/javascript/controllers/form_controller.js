124ed54af9491b8a63e478340eadc570f4588c78
cabbachew <102335309+cabbachew@users.noreply.github.com>
2023-02-19 20:32:16 -0800

    submit search form after each keystroke and hide submit button using stimulus

--- app/javascript/controllers/form_controller.js
+++ app/javascript/controllers/form_controller.js
@@ -1,6 +1,16 @@
 import { Controller } from "@hotwired/stimulus"
 
 export default class extends Controller {
+  static get targets() { return ["submit"] }
+
+  connect() {
+    this.submitTarget.hidden = true
+  }
+
+  submit() {
+    this.submitTarget.click()
+  }
+
   hideValidationMessage(event) {
     event.stopPropagation()
     event.preventDefault()
